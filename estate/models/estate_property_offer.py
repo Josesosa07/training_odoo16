af4ec2033a011669fe936b7f389eb88122a94be4
Jose Sosa <josesl@vauxoo.com>
2023-02-21 17:28:38 -0600

    [IMP] estate: relations onetomany offers to seller

---
+++ estate/models/estate_property_offer.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+_status = [
+    ("accepted", "Accepted"),
+    ("refused", "Refused")
+]
+
+
+class EstatePropertyOffer(models.Model):
+    # ---------------------------------------- Private Attributes ---------------------------------
+    _name = "estate.property.offer"
+    _description = "Real Estate Property Offer"
+
+    # --------------------------------------- Fields Declaration ----------------------------------
+
+    # Basic
+    price = fields.Float()
+    state = fields.Selection(selection=_status, copy=False, string="Status")
+    partner_id = fields.Many2one('res.partner', string='Partner', required=True)
+    property_id = fields.Many2one('estate.property', string='Property', required=True)
