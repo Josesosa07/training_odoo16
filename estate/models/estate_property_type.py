36f7cf2280c3f3d429455d4587cbe20cd803d4cd
Jose Sosa <josesl@vauxoo.com>
2023-02-27 11:06:17 -0600

    [IMP] estate: Add a stat button to property type

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -21,3 +21,13 @@ class EstatePropertyType(models.Model):
 
     # Relational (for inline view)
     property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id", string="Offer")
+    offer_count = fields.Integer("Offers Count", compute="_compute_offer_count", default=0)
+
+    # Compute methods:
+    def _compute_offer_count(self):
+        for record in self:
+            if record.offer_ids:
+                record.offer_count = len(record.offer_ids)
+            else:
+                record.offer_count = 0
