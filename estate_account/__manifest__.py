b79a838493ecd1acd18082e596d7125a8d91409a
Jose Sosa <josesl@vauxoo.com>
2023-02-27 16:42:13 -0600

    [IMP] estate: Create a link module

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Real Estate Accounting",
+    "author": "Vauxoo",
+    "version": "16.0.1.0.0",
+    "category": "Category",
+    "summary": "Estate account",
+    "depends": [
+        "estate",
+        "account",
+    ],
+    "auto_install": True,
+    "license": "LGPL-3",
+}
