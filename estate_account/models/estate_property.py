be8ee1408e2832b26dc5513feeebfb3ca64bd32f
Jose Sosa <josesl@vauxoo.com>
2023-02-28 09:35:01 -0600

    [IMP] estate_account: Add the third step of invoice creation. Add name, quantity, price

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -18,6 +18,22 @@ class EstateProperty(models.Model):
                 "partner_id": record.buyer_id.id,
                 "move_type": "out_invoice",
                 "journal_id": journal.id,
+                "invoice_line_ids": [
+                    Command.create(
+                        {
+                            "name": record.name,
+                            "quantity": 1.0,
+                            "price_unit": record.selling_price * 6.0 / 100.0,
+                        }
+                    ),
+                    Command.create(
+                        {
+                            "name": "Administrative fees",
+                            "quantity": 1.0,
+                            "price_unit": 100.0,
+                        }
+                    ),
+                ],
             }
             moves = self.env["account.move"].create(values)
         return res
