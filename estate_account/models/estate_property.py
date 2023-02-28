08ce69e8c5f448dbd0a373d1fe558c8414c69d28
Jose Sosa <josesl@vauxoo.com>
2023-02-28 09:33:55 -0600

    [IMP] estate_account: Add journal in the invoice creation

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -4,7 +4,6 @@ from odoo import models, Command
 
 
 class EstateProperty(models.Model):
-
     # ---------------------------------------- Private Attributes ---------------------------------
 
     _inherit = "estate.property"
@@ -13,10 +12,12 @@ class EstateProperty(models.Model):
 
     def action_sold(self):
         res = super().action_sold()
+        journal = self.env["account.journal"].search([("type", "=", "sale")], limit=1)
         for record in self:
             values = {
                 "partner_id": record.buyer_id.id,
                 "move_type": "out_invoice",
+                "journal_id": journal.id,
             }
             self.env["account.move"].create(values)
         return res
