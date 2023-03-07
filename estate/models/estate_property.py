5b3dcd6a5e86f6f809ca2ae190a5644219b84263
Jose Sosa <josesl@vauxoo.com>
2023-03-07 11:40:48 -0600

    [IMP] estate: Add company_id

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -58,9 +58,10 @@ class EstateProperty(models.Model):
     # Relational
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
     buyer_id = fields.Many2one("res.partner", string="Buyer", readonly=True, copy=False)
-    user_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
+    user_id = fields.Many2one("res.users", string="Salesman")
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    company_id = fields.Many2one('res.company', required=True, default=lambda self: self.env.company)
 
     # Computed
     total_area = fields.Integer(
